3ece9fa17c4bc8caee13e1969d5ea21d58ad9d5c
Rodrigo Matagawa <rodrigo.mataga@code7.com>
2021-03-21 21:29:57 -0300

    finishing

---
+++ vuejs-cli/src/components/Comments.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <h1>Comentários</h1>
+        <hr/>
+        <FormTodo v-on:add-todo="addComment"></FormTodo>
+        <div class="list-group">
+            <div class="list-group-item" v-for="(comment, index) in allComments" :key="comment">
+                <span class="comment__author">Autor: <strong>{{ comment.name }}</strong></span>
+                <p>{{ comment.message }}</p>
+                <div>
+                    <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)">Excluir</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import FormTodo from './FormTodo'
+
+export default {
+    components: {
+        FormTodo
+    },
+    data: function() {
+        return {
+            comments: []
+        }
+    },
+    methods: {
+        removeComment(index) {
+            this.comments.splice(index, 1);
+        },
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+    },
+    computed: {
+        allComments() {
+            return this.comments.map(comment => ({
+                ...comment,
+                name: comment.name.trim() === '' ? "Anônimo" : comment.name
+            }))
+        }
+    },
+    watch: {
+        comments(value) {
+            console.log(`Value: ${JSON.stringify(value)}`);
+        }
+    }
+}
+    
+</script>
---
+++ vuejs-cli/src/components/FormTodo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="form-todo form-group">
+            <p>
+                <input type="text" placeholder="Nome" class="form-control" v-model="name">
+            </p>
+            <p>
+                <textarea name="message" placeholder="Comentário" class="form-control" v-model="message"></textarea>
+            </p>
+            <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            comments: [],
+            name: "",
+            message: ""
+        }
+    },
+    methods: {
+        addComment() {
+            console.log(`Name: ${this.name} || Message: ${this.message}`)
+            if(this.message.trim() === "") {
+                console.log("The message is empty.")
+                return;
+            }
+
+            this.$emit('add-todo', {
+                name: this.name,
+                message: this.message
+            })
+
+            this.name = '';
+            this.message = '';
+        }
+    }
+}
+</script>
